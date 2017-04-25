9804cb1fcd9342588a4871f505b0542fd3a8e35a
twistezo <luk89@outlook.com>
2017-04-25 14:00:55 +0200

    refactor add FPS counter

--- core/src/com/twistezo/screens/GameScreen.java
+++ core/src/com/twistezo/screens/GameScreen.java
@@ -1,16 +1,20 @@
 package com.twistezo.screens;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.twistezo.NinjaGame;
-import com.twistezo.playground.PlayerFromAtlas;
-import com.twistezo.playground.PlayerFromSheet;
+import com.twistezo.utils.FpsCounter;
+import com.twistezo.characters.Player;
 
 /**
  * @author twistezo (24.04.2017)
  */
 
 public class GameScreen extends AbstractScreen {
-    PlayerFromSheet playerFromSheet;
-    PlayerFromAtlas playerFromAtlas;
+    private Player player;
+    private FpsCounter fpsCounter;
+    private Texture background;
+    private Image backgroundImg;
 
     public GameScreen(NinjaGame game) {
         super(game);
@@ -19,21 +23,30 @@ public class GameScreen extends AbstractScreen {
 
     @Override
     protected void init() {
+        initBackground();
+        initFpsCounter();
         initPlayer();
     }
 
-    private void initPlayer() {
-        playerFromSheet = new PlayerFromSheet();
-        playerFromSheet.setSize(playerFromSheet.getRegionWidth()/2, playerFromSheet.getRegionHeight()/2);
-        playerFromSheet.setPosition(100, 50);
-        playerFromSheet.setDebug(true);
-        stage.addActor(playerFromSheet);
+    private void initBackground() {
+        background = new Texture("game-background.png");
+        backgroundImg = new Image(background);
+        backgroundImg.setPosition(0, 0);
+        stage.addActor(backgroundImg);
+    }
 
-        playerFromAtlas = new PlayerFromAtlas();
-        playerFromAtlas.setSize(playerFromSheet.getRegionWidth()/2, playerFromSheet.getRegionHeight()/2);
-        playerFromAtlas.setPosition(300, 50);
-        playerFromAtlas.setDebug(true);
-        stage.addActor(playerFromAtlas);
+    private void initFpsCounter() {
+        fpsCounter = new FpsCounter();
+        fpsCounter.setPosition(5, 15);
+        stage.addActor(fpsCounter);
+    }
+
+    private void initPlayer() {
+        player = new Player();
+        player.setPosition(50, 50);
+        player.setSize(player.getWidth(), player.getHeight());
+        player.setDebug(true);
+        stage.addActor(player);
     }
 
     @Override
