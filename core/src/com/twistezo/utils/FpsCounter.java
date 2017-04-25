fed3eccdd81a7e63b1b455085484b7d67b355546
twistezo <luk89@outlook.com>
2017-04-25 14:00:32 +0200

    add fps counter class

---
+++ core/src/com/twistezo/utils/FpsCounter.java
@@ -0,0 +1,25 @@
+package com.twistezo.utils;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * @author twistezo (25.04.2017)
+ */
+
+public class FpsCounter extends Actor {
+    BitmapFont font;
+
+    public FpsCounter(){
+        font = new BitmapFont();
+        font.setColor(0,1,0,1);
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+        font.draw(batch, Gdx.graphics.getFramesPerSecond() + " FPS", getX(), getY());
+    }
+}
