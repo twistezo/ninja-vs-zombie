579f77436204362c384370d47ec62a110dd36cbe
twistezo <luk89@outlook.com>
2017-04-28 18:46:47 +0200

    add readme

---
+++ core/src/com/twistezo/utils/GameSettings.java
@@ -0,0 +1,10 @@
+package com.twistezo.utils;
+
+/**
+ * @author twistezo (22.04.2017)
+ */
+
+public class GameSettings {
+    public final static int SCREEN_WIDTH = 800;
+    public final static int SCREEN_HEIGHT = 480;
+}
--- desktop/src/com/twistezo/desktop/DesktopLauncher.java
+++ desktop/src/com/twistezo/desktop/DesktopLauncher.java
@@ -3,7 +3,7 @@ package com.twistezo.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.twistezo.NinjaGame;
-import com.twistezo.old.GameSettings;
+import com.twistezo.utils.GameSettings;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
