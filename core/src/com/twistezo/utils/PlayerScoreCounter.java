be907f93bfdacc4d2784a2a363ed65ae573eb988
twistezo <luk89@outlook.com>
2017-04-29 10:54:33 +0200

    add score counter

---
+++ core/src/com/twistezo/utils/PlayerScoreCounter.java
@@ -0,0 +1,34 @@
+package com.twistezo.utils;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * @author twistezo (25.04.2017)
+ */
+
+public class PlayerScoreCounter extends Actor {
+    private BitmapFont font;
+    private int score = 0;
+
+    public PlayerScoreCounter(){
+        font = new BitmapFont();
+        font.setColor(1,0,0,1);
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+        font.draw(batch, "KILLED ZOMBIES: " + score, getX(), getY());
+    }
+
+    public void addScore(){
+        this.score += 1;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+}
