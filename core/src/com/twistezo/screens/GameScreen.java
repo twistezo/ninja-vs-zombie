4a22e9ebbee5e59dab1b88085411035e78dcaca7
twistezo <luk89@outlook.com>
2017-04-30 20:33:23 +0200

    refactor, fix attack button

--- core/src/com/twistezo/screens/GameScreen.java
+++ core/src/com/twistezo/screens/GameScreen.java
@@ -7,7 +7,9 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.TimeUtils;
 import com.twistezo.NinjaGame;
 import com.twistezo.characters.Player;
+import com.twistezo.characters.Zombie;
 import com.twistezo.characters.ZombieFemale;
+import com.twistezo.characters.ZombieMale;
 import com.twistezo.utils.FpsCounter;
 import com.twistezo.utils.PlayerHealthBar;
 import com.twistezo.utils.PlayerScoreCounter;
@@ -20,9 +22,11 @@ import java.util.Random;
  */
 
 public class GameScreen extends AbstractScreen {
+    private final int SPAWN_TIME = 10 * 1000; //ms
     private Player player;
+    private ZombieMale zombieMale;
     private ZombieFemale zombieFemale;
-    private ArrayList<ZombieFemale> femaleZombies;
+    private ArrayList<Zombie> zombies;
     private FpsCounter fpsCounter;
     private PlayerHealthBar playerHealthBar;
     private PlayerScoreCounter playerScoreCounter;
@@ -37,8 +41,8 @@ public class GameScreen extends AbstractScreen {
     private Texture buttonLeftTexture;
     private Texture buttonRightTexture;
     private Texture buttonAttackTexture;
+    private Random random;
     private long lastZombieTime;
-    private int spawnTime = 10000; //ms
     private float timeSinceCollision = 0;
     private float timeSinceDeath = 0;
     private boolean isCollision = false;
@@ -59,7 +63,8 @@ public class GameScreen extends AbstractScreen {
         initPlayerScoreCounter();
         initPlayer();
         initButtonsListeners();
-        femaleZombies = new ArrayList<>();
+        zombies = new ArrayList<>();
+        random = new Random();
     }
 
     private void initBackground() {
@@ -107,7 +112,6 @@ public class GameScreen extends AbstractScreen {
                 toggleDebugMode(isDebugMode);
             }
         });
-
         buttonLeft.addListener(new ClickListener() {
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -123,7 +127,6 @@ public class GameScreen extends AbstractScreen {
                 player.doMovement("IDLE");
             }
         });
-
         buttonRight.addListener(new ClickListener() {
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -139,20 +142,12 @@ public class GameScreen extends AbstractScreen {
                 player.doMovement("IDLE");
             }
         });
-
         buttonAttack.addListener(new ClickListener() {
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 player.setAttacking(true);
                 player.doMovement("ATTACK");
                 return super.touchDown(event, x, y, pointer, button);
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                super.touchUp(event, x, y, pointer, button);
-                player.setAttacking(false);
-                player.doMovement("IDLE");
             }
         });
     }
@@ -170,15 +165,20 @@ public class GameScreen extends AbstractScreen {
     }
 
     private void initPlayer() {
-        player = new Player();
-        player.setPosition(300, 50);
-        player.setSize(player.getWidth(), player.getHeight());
+        player = new Player(300, 50);
         stage.addActor(player);
     }
 
-    private void spawnFemaleZombie(){
-        Random random = new Random();
-        femaleZombies.add(new ZombieFemale(random.nextBoolean()));
+    private void spawnZombies(){
+        int switcher = random.nextInt(2);
+        switch(switcher) {
+            case 0:
+                zombies.add(new ZombieMale(random.nextBoolean()));
+                break;
+            case 1:
+                zombies.add(new ZombieFemale(random.nextBoolean()));
+                break;
+        }
         lastZombieTime = TimeUtils.millis();
     }
 
@@ -189,71 +189,81 @@ public class GameScreen extends AbstractScreen {
         timeSinceDeath += delta;
         update();
         stage.draw();
-        if (TimeUtils.millis() - lastZombieTime > spawnTime) {
-            spawnFemaleZombie();
-        }
-        for(ZombieFemale zombie : femaleZombies) {
-            stage.addActor(zombie);
-        }
-        if(timeSinceCollision > 0.75f) {
-            checkCollision();
-            timeSinceCollision = 0;
-        }
-        zombieFollowPlayerX();
-        checkZombieDeath();
-        if(timeSinceDeath > 2f) {
-            removeDeathZombie();
-            timeSinceDeath = 0;
-        }
-        checkPlayerDead();
 
+        if(!player.isDead()) {
+            if (TimeUtils.millis() - lastZombieTime > SPAWN_TIME) {
+                spawnZombies();
+            }
+            for(Zombie zombie : zombies) {
+                stage.addActor(zombie);
+            }
+            if(timeSinceCollision > 0.75f) {
+                checkCollision();
+                timeSinceCollision = 0;
+            }
+            zombieFollowPlayerX();
+            checkZombieDeath();
+            if(timeSinceDeath > 2f) {
+                removeDeathZombie();
+                timeSinceDeath = 0;
+            }
+        }
+        checkPlayerDeath();
+        moveActorsToFront();
+    }
+
+    private void moveActorsToFront() {
+        player.toFront();
+        buttonLeft.toFront();
+        buttonRight.toFront();
+        buttonAttack.toFront();
     }
 
     private void checkCollision() {
-        for(ZombieFemale zombieFemale : femaleZombies) {
-            if(player.getBounds().overlaps(zombieFemale.getBounds())){
-                zombieFemale.setInEnemyBounds(true);
+        for(Zombie zombie : zombies) {
+            if(player.getBounds().overlaps(zombie.getBounds())){
+                zombie.setInEnemyBounds(true);
                 player.setInEnemyBounds(true);
-                if(!player.isAttacking()) {
+                if(player.isAttacking()) {
+                    zombie.decreaseHealth();
+                } else {
                     playerHealthBar.reducePlayerHealth();
                 }
-                if(player.isAttacking()) {
-                    zombieFemale.decreaseHealth();
-                }
-            } else if (!player.getBounds().overlaps(zombieFemale.getBounds())){
-                zombieFemale.setInEnemyBounds(false);
+            } else {
+                zombie.setInEnemyBounds(false);
                 player.setInEnemyBounds(false);
             }
         }
     }
 
     private void checkZombieDeath() {
-        for(int i=0; i<femaleZombies.size(); i++) {
-            if(femaleZombies.get(i).getHealth() <= 0) {
-                femaleZombies.get(i).setDeath(true);
+        for(int i=0; i<zombies.size(); i++) {
+            if(zombies.get(i).getHealth() <= 0) {
+                zombies.get(i).setDeath(true);
             }
         }
     }
 
     private void removeDeathZombie() {
-        for(int i=0; i<femaleZombies.size(); i++) {
-            if(femaleZombies.get(i).isReadyToRemove() && femaleZombies.get(i).isDeath()){
+        for(int i=0; i<zombies.size(); i++) {
+            if(zombies.get(i).isReadyToRemove() && zombies.get(i).isDeath()){
                 playerScoreCounter.addScore();
-                femaleZombies.get(i).remove();
-                femaleZombies.remove(i);
+                zombies.get(i).remove();
+                zombies.remove(i);
             }
         }
     }
 
-    private void checkPlayerDead() {
+    private void checkPlayerDeath() {
         if(playerHealthBar.getPlayerHealth() <= 0) {
             player.setDead(true);
+            playerHealthBar.setHealth(0);
         }
     }
 
     private void zombieFollowPlayerX() {
-        for(ZombieFemale zombieFemale : femaleZombies) {
-            zombieFemale.setWalkTargetX(player.getPlayerCurrentX());
+        for(Zombie zombie : zombies) {
+            zombie.setWalkTargetX(player.getPlayerCurrentX());
         }
     }
 
@@ -269,9 +279,9 @@ public class GameScreen extends AbstractScreen {
     private void toggleDebugMode(boolean value) {
         player.setDebug(value);
         player.setDebugMode(value);
-        for (ZombieFemale zombieFemale : femaleZombies) {
-            zombieFemale.setDebug(value);
-            zombieFemale.setDebugMode(value);
+        for (Zombie zombie : zombies) {
+            zombie.setDebug(value);
+            zombie.setDebugMode(value);
         }
     }
 }
