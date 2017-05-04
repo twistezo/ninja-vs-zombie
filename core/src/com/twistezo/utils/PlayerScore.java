cbb0ecc1ef54a7c1a9446bc85d9b429fad025d5b
twistezo <luk89@outlook.com>
2017-05-04 10:28:13 +0200

    add PlayerScore class for highscore screen

---
+++ core/src/com/twistezo/utils/PlayerScore.java
@@ -0,0 +1,31 @@
+package com.twistezo.utils;
+
+/**
+ * @author twistezo (03.05.2017)
+ */
+
+public class PlayerScore {
+    private String name;
+    private int score;
+
+    public PlayerScore(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public String toString() {
+        return "PlayerScore{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+}
